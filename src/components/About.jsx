c269fa8edf6f9ac0501eb17d08c74c3433d47b5b
Serezha <volos11223344@yandex.ru>
2025-08-01 23:22:30 +0300

    fix: исправил секцию about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,53 +7,43 @@ import { styles } from '../styles';
 import { fadeIn, textVariant } from '../utils/motion';
 
 const ServiceCard = ({ index, title, icon }) => (
-  <Tilt className="xs:w-[250px] w-full">
-    <motion.div
-      variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
-      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
-    >
-      <div
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
-      >
-        <img src={icon} alt="web-development" className="w-16 h-16 object-contain" loading="lazy" />
+	<Tilt className="xs:w-[250px] w-full">
+		<motion.div variants={fadeIn('right', 'spring', index * 0.5, 0.75)} className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card">
+			<div
+				options={{
+					max: 45,
+					scale: 1,
+					speed: 450,
+				}}
+				className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+				<img src={icon} alt="web-development" className="w-16 h-16 object-contain" loading="lazy" />
 
-        <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
-      </div>
-    </motion.div>
-  </Tilt>
+				<h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+			</div>
+		</motion.div>
+	</Tilt>
 );
 
 const About = () => {
-  return (
-    <>
-      <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview</h2>
-      </motion.div>
+	return (
+		<>
+			<motion.div variants={textVariant()}>
+				{/* <p className={styles.sectionSubText}>Introduction</p> */}
+				<h2 className={styles.sectionHeadText}>Обо мне</h2>
+			</motion.div>
 
-      <motion.p
-        variants={fadeIn('', '', 0.1, 1)}
-        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
-      >
-        I am a Computer Science student at SRM University and multilingual speaker with a passion
-        for ethical hacking, artificial intelligence, and neural language processing. I am looking
-        to join a company that values innovation, collaboration, and continuous learning and am
-        confident that my enthusiasm and drive will help me succeed in any role that leverages my
-        passion for technology.
-      </motion.p>
+			<motion.p variants={fadeIn('', '', 0.1, 1)} className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
+				Я фронтенд-разработчик с более чем 4-летним опытом. Начинал как верстальщик лендингов в небольшой веб-студии, затем меня увлекли более продвинутые технологии. В настоящее время я занимаюсь
+				полноценной разработкой сайтов с использованием современных фреймворков, паттернов и технологий.
+			</motion.p>
 
-      <div className="mt-20 flex flex-wrap gap-10">
-        {services.map((service, index) => (
-          <ServiceCard key={service.title} index={index} {...service} />
-        ))}
-      </div>
-    </>
-  );
+			<div className="mt-20 flex flex-wrap gap-10">
+				{services.map((service, index) => (
+					<ServiceCard key={service.title} index={index} {...service} />
+				))}
+			</div>
+		</>
+	);
 };
 
 const WrappedAbout = SectionWrapper(About, 'about');
